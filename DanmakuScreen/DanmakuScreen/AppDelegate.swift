12bbe8f55cc075244665f2e9ad33f0d0c877e510
eric3u <eric3u@outlook.com>
2019-04-08 00:43:45 +0800

    [Feat] Init Project

---
+++ DanmakuScreen/DanmakuScreen/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DanmakuScreen
+//
+//  Created by CmST0us on 2019/4/8.
+//  Copyright © 2019 eric3u. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
